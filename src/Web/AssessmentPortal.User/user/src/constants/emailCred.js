b4ceb7b1a1978bf955fca2899cf10d973cd03a9a
Rajeesh Durai <127297740+Rajeesh-Durai@users.noreply.github.com>
2024-01-29 10:17:55 +0530

    Update emailCred.js

--- src/Web/AssessmentPortal.User/user/src/constants/emailCred.js
+++ src/Web/AssessmentPortal.User/user/src/constants/emailCred.js
@@ -1,3 +1,3 @@
-export const EMAILJS_SERVICE_ID = "service_updw4cq";
-export const EMAILJS_TEMPLATE_ID = "template_wfhzgvs";
-export const EMAILJS_PUBLIC_KEY= "IkWbOj8hUcy1g8AWr";
+export const EMAILJS_SERVICE_ID = "your service id";
+export const EMAILJS_TEMPLATE_ID = "your template id";
+export const EMAILJS_PUBLIC_KEY= "your key";
